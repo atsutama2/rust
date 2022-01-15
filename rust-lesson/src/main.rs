01c7fa5db01f07888c5dedccbf8a2a66f22dce10
atsutama2 <atsutama.0713@gmail.com>
2022-01-15 13:41:01 +0900

    study rust

--- rust-lesson/src/main.rs
+++ rust-lesson/src/main.rs
@@ -1,8 +1,18 @@
-mod vars;
-
+// mod stack_heap;
+// mod vars;
+// mod ownership;
+// mod generics;
+mod lifetime;
 fn main() {
-    println!("Hello, world!");
-    vars::run();
-    vars::sub_a::fnc_a();
-    vars::sub_b::fnc_b();
+    // println!("Hello, world!");
+    // vars::run();
+    // stack_heap::run();
+    // vars::sub_a::fnc_a();
+    // vars::sub_b::fnc_b();
+
+    // ownership::run();
+
+    // generics::run();
+
+    lifetime::run();
 }
